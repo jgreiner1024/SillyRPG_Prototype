00b1269dc1de68357057e8e4e3c9223abe5477cd
Jeremy Greiner <jgreiner@1024studios.com>
2025-08-17 15:45:42 -0500

    broke the default rules into 2 seperate awaits.

--- index.js
+++ index.js
@@ -138,7 +138,9 @@ async function setRulesExtensionPrompt() {
             }
 
             //update the charaNote with the default rules
-            const defaultrules = (await fetch('./scripts/extensions/third-party/SillyRPG_Prototype/defaultrules.json')).json();
+            const defaultRulesResponse = await fetch('./scripts/extensions/third-party/SillyRPG_Prototype/defaultrules.json');
+            const defaultrules = await defaultRulesResponse.json();
+            
             charaNote.prompt = yaml.stringify(defaultrules);
             charaNote.useChara = false; //we don't actually want to use the charaNote, we are just using it for storage and editing until we build a real UI.
             context.saveSettingsDebounced();
